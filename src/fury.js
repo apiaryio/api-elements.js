e6dbeebb3a47b7cbf30c38f1d747e497264fc943
Z <z@apiary.io>
2015-03-03 20:21:21 +0100

    Fix Markdown renderer name

--- src/fury.js
+++ src/fury.js
@@ -3,6 +3,6 @@ var fury = {}
 // Legacy Interface
 fury.legacyAPI = require('./legacy/blueprint');
 fury.legacyBlueprintParser = require('./legacy/blueprint-parser');
-fury.legacyMarkdown = require('./legacy/markdown')
+fury.legacyMarkdownRenderer = require('./legacy/markdown')
 
 module.exports = fury;
