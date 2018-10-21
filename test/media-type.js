9f9b651848d8a814f73e23791f624cc446ee6a8a
Kyle Fuller <kyle@fuller.li>
2018-10-21 02:07:38 +0000

    fix(package): update media-typer to version 1.0.0

--- test/media-type.js
+++ test/media-type.js
@@ -2,9 +2,9 @@
 // Allows chai `expect(true).to.be.true;`
 
 import { expect } from 'chai';
-import { isTextContentType } from '../src/media-type';
+import { isTextContentType, isMultiPartFormData, hasBoundary, parseBoundary } from '../src/media-type';
 
-describe('isTextContentType', () => {
+describe('#isTextContentType', () => {
   it('does not detect non-text content type', () => {
     expect(isTextContentType('application/json')).to.be.false;
   });
@@ -23,5 +23,43 @@ describe('isTextContentType', () => {
 
   it('detects text content type with version', () => {
     expect(isTextContentType('text/plain; version=1')).to.be.true;
+  });
+});
+
+describe('#isMultiPartFormData', () => {
+  it('does not detect non multipart form content type', () => {
+    expect(isMultiPartFormData('application/json')).to.be.false;
+  });
+
+  it('does not detect invalid content type', () => {
+    expect(isMultiPartFormData('')).to.be.false;
+  });
+
+  it('detects multipart/form-data', () => {
+    expect(isMultiPartFormData('multipart/form-data')).to.be.true;
+  });
+
+  it('detects text multipart/form-data with version', () => {
+    expect(isMultiPartFormData('multipart/form-data; BOUNDARY=ABC')).to.be.true;
+  });
+});
+
+describe('#hasBoundary', () => {
+  it('returns false when the given content type does not have a boundary', () => {
+    expect(hasBoundary('multipart/form-data')).to.be.false;
+  });
+
+  it('returns true when the given content type has a boundary', () => {
+    expect(hasBoundary('multipart/form-data; BOUNDARY=ABC')).to.be.true;
+  });
+});
+
+describe('#parseBoundary', () => {
+  it('defaults to BOUNDARY when no boundary is provided', () => {
+    expect(parseBoundary('multipart/form-data')).to.equal('BOUNDARY');
+  });
+
+  it('parses the boundary from a content type', () => {
+    expect(parseBoundary('multipart/form-data; BOUNDARY=ABC')).to.equal('ABC');
   });
 });
