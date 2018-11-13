bf0dd2f5b9411bb7d070f1f6d819dee550843329
Kyle Fuller <kyle@fuller.li>
2018-11-12 17:27:06 -0800

    feat: Provide warning for unsupported keys in OpenAPI Object

--- packages/fury-adapter-oas3-parser/lib/parser/root.js
+++ packages/fury-adapter-oas3-parser/lib/parser/root.js
@@ -5,6 +5,14 @@ const { createError, createWarning } = require('../elements');
 const parseOpenAPI = require('./openapi');
 
 const requiredKeys = ['openapi', 'info', 'paths'];
+const unsupportedKeys = ['components', 'servers', 'security', 'tags', 'externalDocs'];
+const hasKey = R.curry((key, member) => member.key.toValue() === key);
+const isUnsupportedKey = R.anyPass(R.map(hasKey, unsupportedKeys));
+
+const createUnsupportedMemberWarning = R.curry((minim, member) => {
+  const message = `OpenAPI Object contains unsupported key '${member.key.toValue()}'`;
+  return createWarning(minim, message, member.key);
+});
 
 const createInvalidMemberWarning = R.curry((minim, member) => {
   const message = `OpenAPI Object contains invalid key '${member.key.toValue()}'`;
@@ -28,16 +36,18 @@ function parseOASObject(minim, object) {
 
   // Parse object members
 
-  const hasKey = R.curry((key, member) => member.key.toValue() === key);
   const isUnhandledKey = R.anyPass([
     hasKey('info'),
     hasKey('paths'),
   ]);
+
   const parseMember = R.cond([
     [hasKey('openapi'), parseOpenAPI(minim)],
 
     // FIXME Ignoring `info` and `path` keys
     [isUnhandledKey, () => new minim.elements.ParseResult()],
+
+    [isUnsupportedKey, createUnsupportedMemberWarning(minim)],
 
     // Return a warning for every other key
     [R.T, createInvalidMemberWarning(minim)],
--- packages/fury-adapter-oas3-parser/test/parser/test_root.js
+++ packages/fury-adapter-oas3-parser/test/parser/test_root.js
@@ -54,6 +54,90 @@ describe('#parseOASObject', function () {
     expect(result.errors.get(0).toValue()).to.equal("OpenAPI Object is missing required property 'paths'");
   });
 
+  it('provides warning for unsupported keys', function () {
+    const object = new minim.elements.Object({
+      openapi: '3.0.0',
+      info: {},
+      paths: {},
+      invalid: {},
+    });
+
+    const result = parseOASObject(minim, object);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("OpenAPI Object contains invalid key 'invalid'");
+  });
+
+  it('provides warning for unsupported security key', function () {
+    const object = new minim.elements.Object({
+      openapi: '3.0.0',
+      info: {},
+      paths: {},
+      security: {},
+    });
+
+    const result = parseOASObject(minim, object);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("OpenAPI Object contains unsupported key 'security'");
+  });
+
+  it('provides warning for unsupported tags key', function () {
+    const object = new minim.elements.Object({
+      openapi: '3.0.0',
+      info: {},
+      paths: {},
+      tags: [],
+    });
+
+    const result = parseOASObject(minim, object);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("OpenAPI Object contains unsupported key 'tags'");
+  });
+
+  it('provides warning for unsupported externalDocs key', function () {
+    const object = new minim.elements.Object({
+      openapi: '3.0.0',
+      info: {},
+      paths: {},
+      externalDocs: {},
+    });
+
+    const result = parseOASObject(minim, object);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("OpenAPI Object contains unsupported key 'externalDocs'");
+  });
+
+  it('provides warning for unsupported components key', function () {
+    const object = new minim.elements.Object({
+      openapi: '3.0.0',
+      info: {},
+      paths: {},
+      components: {},
+    });
+
+    const result = parseOASObject(minim, object);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("OpenAPI Object contains unsupported key 'components'");
+  });
+
+  it('provides warning for unsupported servers key', function () {
+    const object = new minim.elements.Object({
+      openapi: '3.0.0',
+      info: {},
+      paths: {},
+      servers: [],
+    });
+
+    const result = parseOASObject(minim, object);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("OpenAPI Object contains unsupported key 'servers'");
+  });
+
   it('provides warning for invalid keys', function () {
     const object = new minim.elements.Object({
       openapi: '3.0.0',
