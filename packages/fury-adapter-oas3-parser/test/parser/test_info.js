133e29519f6738df7f763a0f52e08034dad4ff2b
Kyle Fuller <kyle@fuller.li>
2018-11-19 16:25:59 -0800

    test: Wrap info test sections in describe block

--- packages/fury-adapter-oas3-parser/test/parser/test_info.js
+++ packages/fury-adapter-oas3-parser/test/parser/test_info.js
@@ -15,137 +15,135 @@ describe('#parseInfo', function () {
     expect(result.errors.get(0).toValue()).to.equal("'Info Object' is not an object");
   });
 
-  // Required Properties
+  describe('missing required properties', function () {
+    it('provides error for missing title', function () {
+      const info = new minim.elements.Object({
+        version: '1.0.0',
+      });
 
-  it('provides error for missing title', function () {
-    const info = new minim.elements.Object({
-      version: '1.0.0',
+      const result = parseInfo(minim, info);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal("'Info Object' is missing required property 'title'");
     });
 
-    const result = parseInfo(minim, info);
+    it('provides error for missing version', function () {
+      const info = new minim.elements.Object({
+        title: 'My API',
+      });
 
-    expect(result.length).to.equal(1);
-    expect(result.errors.get(0).toValue()).to.equal("'Info Object' is missing required property 'title'");
+      const result = parseInfo(minim, info);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal("'Info Object' is missing required property 'version'");
+    });
   });
 
-  it('provides error for missing version', function () {
-    const info = new minim.elements.Object({
-      title: 'My API',
+  describe('required property type checking', function () {
+    it('provides error when title is non-string', function () {
+      const info = new minim.elements.Object({
+        title: 1,
+        version: '1.0.0',
+      });
+
+      const result = parseInfo(minim, info);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal("'Info Object' 'title' is not a string");
     });
 
-    const result = parseInfo(minim, info);
+    it('provides error when version is non-string', function () {
+      const info = new minim.elements.Object({
+        title: 'My API',
+        version: 1,
+      });
 
-    expect(result.length).to.equal(1);
-    expect(result.errors.get(0).toValue()).to.equal("'Info Object' is missing required property 'version'");
+      const result = parseInfo(minim, info);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal("'Info Object' 'version' is not a string");
+    });
   });
 
-  // Required Property Type Check
+  describe('optional property type checking', function () {
+    it('provides warning when description is non-string', function () {
+      const info = new minim.elements.Object({
+        title: 'My API',
+        version: '1.0.0',
+        description: 1,
+      });
 
-  it('provides error when title is non-string', function () {
-    const info = new minim.elements.Object({
-      title: 1,
-      version: '1.0.0',
+      const result = parseInfo(minim, info);
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal("'Info Object' 'description' is not a string");
+    });
+  });
+
+  describe('warnings for unsupported properties', function () {
+    it('provides warning for unsupported termsOfService key', function () {
+      const object = new minim.elements.Object({
+        title: 'My API',
+        version: '1.0.0',
+        termsOfService: '',
+      });
+
+      const result = parseInfo(minim, object);
+
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains unsupported key 'termsOfService'");
     });
 
-    const result = parseInfo(minim, info);
+    it('provides warning for unsupported contact key', function () {
+      const object = new minim.elements.Object({
+        title: 'My API',
+        version: '1.0.0',
+        contact: {},
+      });
 
-    expect(result.length).to.equal(1);
-    expect(result.errors.get(0).toValue()).to.equal("'Info Object' 'title' is not a string");
-  });
+      const result = parseInfo(minim, object);
 
-  it('provides error when version is non-string', function () {
-    const info = new minim.elements.Object({
-      title: 'My API',
-      version: 1,
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains unsupported key 'contact'");
     });
 
-    const result = parseInfo(minim, info);
+    it('provides warning for unsupported license key', function () {
+      const object = new minim.elements.Object({
+        title: 'My API',
+        version: '1.0.0',
+        license: {},
+      });
 
-    expect(result.length).to.equal(1);
-    expect(result.errors.get(0).toValue()).to.equal("'Info Object' 'version' is not a string");
-  });
+      const result = parseInfo(minim, object);
 
-  // Type Check Optional Properties
-
-  it('provides warning when description is non-string', function () {
-    const info = new minim.elements.Object({
-      title: 'My API',
-      version: '1.0.0',
-      description: 1,
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains unsupported key 'license'");
     });
 
-    const result = parseInfo(minim, info);
-    expect(result.warnings.length).to.equal(1);
-    expect(result.warnings.get(0).toValue()).to.equal("'Info Object' 'description' is not a string");
-  });
+    it('does not provide warning for Info Object extensions', function () {
+      const object = new minim.elements.Object({
+        title: 'My API',
+        version: '1.0.0',
+        'x-extension': {},
+      });
 
-  // Warning for unsupported properties
+      const result = parseInfo(minim, object);
 
-  it('provides warning for unsupported termsOfService key', function () {
-    const object = new minim.elements.Object({
-      title: 'My API',
-      version: '1.0.0',
-      termsOfService: '',
+      expect(result.annotations.length).to.equal(0);
     });
 
-    const result = parseInfo(minim, object);
+    it('provides warning for invalid keys', function () {
+      const object = new minim.elements.Object({
+        title: 'My API',
+        version: '1.0.0',
+        invalid: {},
+      });
 
-    expect(result.warnings.length).to.equal(1);
-    expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains unsupported key 'termsOfService'");
-  });
+      const result = parseInfo(minim, object);
 
-  it('provides warning for unsupported contact key', function () {
-    const object = new minim.elements.Object({
-      title: 'My API',
-      version: '1.0.0',
-      contact: {},
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains invalid key 'invalid'");
     });
-
-    const result = parseInfo(minim, object);
-
-    expect(result.warnings.length).to.equal(1);
-    expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains unsupported key 'contact'");
   });
-
-  it('provides warning for unsupported license key', function () {
-    const object = new minim.elements.Object({
-      title: 'My API',
-      version: '1.0.0',
-      license: {},
-    });
-
-    const result = parseInfo(minim, object);
-
-    expect(result.warnings.length).to.equal(1);
-    expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains unsupported key 'license'");
-  });
-
-  it('does not provide warning for Info Object extensions', function () {
-    const object = new minim.elements.Object({
-      title: 'My API',
-      version: '1.0.0',
-      'x-extension': {},
-    });
-
-    const result = parseInfo(minim, object);
-
-    expect(result.annotations.length).to.equal(0);
-  });
-
-  it('provides warning for invalid keys', function () {
-    const object = new minim.elements.Object({
-      title: 'My API',
-      version: '1.0.0',
-      invalid: {},
-    });
-
-    const result = parseInfo(minim, object);
-
-    expect(result.warnings.length).to.equal(1);
-    expect(result.warnings.get(0).toValue()).to.equal("'Info Object' contains invalid key 'invalid'");
-  });
-
-  // Other
 
   it('provides api category with title and version', function () {
     const info = new minim.elements.Object({
