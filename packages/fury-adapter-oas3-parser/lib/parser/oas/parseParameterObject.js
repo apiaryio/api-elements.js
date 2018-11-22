79a08fc9f3327d19d5b0f8920bc26c89ff4a3bb3
Kyle Fuller <kyle@fuller.li>
2018-11-21 16:31:08 -0800

    feat: Add base Parameter Object parser

---
+++ packages/fury-adapter-oas3-parser/lib/parser/oas/parseParameterObject.js
@@ -0,0 +1,75 @@
+const R = require('ramda');
+const { isObject, isExtension, hasKey } = require('../../predicates');
+const {
+  createError,
+  createWarning,
+  createUnsupportedMemberWarning,
+  createInvalidMemberWarning,
+  validateObjectContainsRequiredKeys,
+} = require('../annotations');
+const pipeParseResult = require('../../pipeParseResult');
+const parseObject = require('../parseObject');
+const parseString = require('../parseString');
+
+const name = 'Parameter Object';
+const requiredKeys = ['name', 'in'];
+const unsupportedKeys = [
+  // FIXME Only contains "fixed" fields from spec
+  'required', 'deprecated', 'allowEmptyValue',
+];
+const isUnsupportedKey = R.anyPass(R.map(hasKey, unsupportedKeys));
+
+const hasValue = R.curry((value, member) => member.value.toValue() === value);
+const isValidInValue = R.anyPass([
+  hasValue('query'),
+  hasValue('header'),
+  hasValue('path'),
+  hasValue('cookie'),
+]);
+
+/**
+ * Parse Parameter Object
+ *
+ * @param minim {Namespace}
+ * @param element {Element}
+ * @returns ParseResult
+ *
+ * @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#parameterObject
+ */
+function parseParameterObject(minim, object) {
+  const validateIn = R.unless(isValidInValue, createError(minim,
+    "'Parameter Object' 'in' must be either 'query, 'header', 'path' or 'cookie'"));
+
+  const parseMember = R.cond([
+    [hasKey('name'), parseString(minim, name, true)],
+    [hasKey('in'), pipeParseResult(minim, parseString(minim, name, true), validateIn)],
+    [hasKey('description'), parseString(minim, name, false)],
+
+    [isUnsupportedKey, createUnsupportedMemberWarning(minim, name)],
+
+    // FIXME Support exposing extensions into parse result
+    [isExtension, () => []],
+
+    // Return a warning for additional properties
+    [R.T, createInvalidMemberWarning(minim, name)],
+  ]);
+
+  const parseParameter = pipeParseResult(minim,
+    R.unless(isObject, createWarning(minim, `'${name}' is not an object`)),
+    validateObjectContainsRequiredKeys(minim, name, requiredKeys),
+    parseObject(minim, parseMember),
+    (parameter) => {
+      const member = new minim.elements.Member(parameter.get('name'));
+
+      const description = parameter.get('description');
+      if (description) {
+        member.description = description;
+      }
+
+      return member;
+    });
+
+  return parseParameter(object);
+}
+
+module.exports = R.curry(parseParameterObject);
---
+++ packages/fury-adapter-oas3-parser/test/parser/oas/test_parseParameterObject.js
@@ -0,0 +1,171 @@
+const { expect } = require('chai');
+const { Fury } = require('fury');
+const parse = require('../../../lib/parser/oas/parseParameterObject');
+
+const { minim } = new Fury();
+
+describe('Parameter Object', () => {
+  it('provides warning when parameter is non-object', () => {
+    const operation = new minim.elements.String();
+
+    const result = parse(minim, operation);
+
+    expect(result.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal(
+      "'Parameter Object' is not an object"
+    );
+  });
+
+  describe('#name', () => {
+    it('provides an error when name is not a string', () => {
+      const parameter = new minim.elements.Object({
+        name: 1,
+        in: 'path',
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal(
+        "'Parameter Object' 'name' is not a string"
+      );
+    });
+  });
+
+  describe('#in', () => {
+    it('provides an error when in is not a string', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 1,
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal(
+        "'Parameter Object' 'in' is not a string"
+      );
+    });
+
+    it('provides an error when in is a permitted value', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'space',
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.length).to.equal(1);
+      expect(result.errors.get(0).toValue()).to.equal(
+        "'Parameter Object' 'in' must be either 'query, 'header', 'path' or 'cookie'"
+      );
+    });
+  });
+
+  describe('#description', () => {
+    it('attaches description to member', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'query',
+        description: 'an example parameter',
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.length).to.equal(1);
+      expect(result.get(0)).to.be.instanceof(minim.elements.Member);
+      expect(result.get(0).description.toValue()).to.equal(
+        'an example parameter'
+      );
+    });
+
+    it('provides a warning when description is not a string', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'query',
+        description: true,
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal(
+        "'Parameter Object' 'description' is not a string"
+      );
+    });
+  });
+
+  describe('warnings for unsupported properties', () => {
+    it('provides warning for unknown required property', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'path',
+        required: true,
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal(
+        "'Parameter Object' contains unsupported key 'required'"
+      );
+    });
+
+    it('provides warning for unknown deprecated property', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'path',
+        deprecated: true,
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal(
+        "'Parameter Object' contains unsupported key 'deprecated'"
+      );
+    });
+
+    it('provides warning for unknown deprecated property', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'path',
+        allowEmptyValue: true,
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.warnings.length).to.equal(1);
+      expect(result.warnings.get(0).toValue()).to.equal(
+        "'Parameter Object' contains unsupported key 'allowEmptyValue'"
+      );
+    });
+
+    it('does not provide warning/errors for extensions', () => {
+      const parameter = new minim.elements.Object({
+        name: 'example',
+        in: 'path',
+        'x-extension': '',
+      });
+
+      const result = parse(minim, parameter);
+
+      expect(result.annotations.isEmpty).to.be.true;
+    });
+  });
+
+  it('provides warning for invalid keys', () => {
+    const parameter = new minim.elements.Object({
+      name: 'example',
+      in: 'path',
+      invalid: '',
+    });
+
+    const result = parse(minim, parameter);
+
+    expect(result.warnings.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal(
+      "'Parameter Object' contains invalid key 'invalid'"
+    );
+  });
+});
