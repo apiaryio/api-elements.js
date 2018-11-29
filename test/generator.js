d8ba260742b6bcc308ead370aa8d2c28ed16dbea
Kyle Fuller <kyle@fuller.li>
2018-11-29 11:31:14 -0800

    test: Enable schema with references generation test

--- test/generator.js
+++ test/generator.js
@@ -93,8 +93,7 @@ describe('bodyFromSchema', () => {
     expect(body.length).to.equal(5);
   });
 
-  // FIXME: Disabled, see https://github.com/apiaryio/fury-adapter-swagger/pull/212
-  xit('can generate a structure with references', () => {
+  it('can generate a structure with references', () => {
     const schema = {
       type: 'array',
       items: {
