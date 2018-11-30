71126e3e6c634f9d9c0c9cdf4278dc3d9a6106a6
Kyle Fuller <kyle@fuller.li>
2018-11-29 16:50:31 -0800

    feat(schema object): Validate given schema is an object

--- packages/fury-adapter-oas3-parser/lib/parser/oas/parseSchemaObject.js
+++ packages/fury-adapter-oas3-parser/lib/parser/oas/parseSchemaObject.js
@@ -1,5 +1,7 @@
 const R = require('ramda');
 const { createWarning } = require('../annotations');
+const pipeParseResult = require('../../pipeParseResult');
+const { isObject } = require('../../predicates');
 
 const name = 'Schema Object';
 
@@ -13,8 +15,14 @@ const name = 'Schema Object';
  * @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#schemaObject
  */
 function parseSchemaObject(minim, member) {
-  const unsupportedAnnotation = createWarning(minim, `'${name}' is unsupported`, member);
-  return new minim.elements.ParseResult([unsupportedAnnotation]);
+  const parseSchema = pipeParseResult(minim,
+    R.unless(isObject, createWarning(minim, `'${name}' is not an object`)),
+    (object) => {
+      const unsupportedAnnotation = createWarning(minim, `'${name}' is unsupported`, object);
+      return new minim.elements.ParseResult([unsupportedAnnotation]);
+    });
+
+  return parseSchema(member.value);
 }
 
 module.exports = R.curry(parseSchemaObject);
--- packages/fury-adapter-oas3-parser/test/parser/oas/parseSchemaObject-test.js
+++ packages/fury-adapter-oas3-parser/test/parser/oas/parseSchemaObject-test.js
@@ -5,6 +5,14 @@ const parse = require('../../../lib/parser/oas/parseSchemaObject');
 const { minim } = new Fury();
 
 describe('Schema Object', () => {
+  it('provides a warning when schema is non-object', () => {
+    const schema = new minim.elements.Member('User', 'my schema');
+    const result = parse(minim, schema);
+
+    expect(result.length).to.equal(1);
+    expect(result.warnings.get(0).toValue()).to.equal("'Schema Object' is not an object");
+  });
+
   it('returns an unsupported warning', () => {
     const schema = new minim.elements.Member('User', {});
     const result = parse(minim, schema);
