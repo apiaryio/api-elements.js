e500b0d57581b6fe1832dce2ee938bbe2498b22c
Kyle Fuller <kyle@fuller.li>
2018-12-10 17:36:11 -0800

    test: Don't use async/await to maintain Node 6 support

--- packages/fury-adapter-oas3-parser/test/integration/parse-test.js
+++ packages/fury-adapter-oas3-parser/test/integration/parse-test.js
@@ -11,20 +11,21 @@ fury.use(adapter);
 describe('#parse', () => {
   const parse = promisify(fury.parse.bind(fury));
 
-  it('can parse petstore example', async () => {
+  it('can parse petstore example', () => {
     const file = path.join(__dirname, 'fixtures', 'petstore');
     const source = fs.readFileSync(`${file}.yaml`, 'utf-8');
 
-    const parseResult = await parse({ source });
-    expect(parseResult).to.be.instanceof(fury.minim.elements.ParseResult);
+    return parse({ source }).then((parseResult) => {
+      expect(parseResult).to.be.instanceof(fury.minim.elements.ParseResult);
 
-    const result = JSON.stringify(fury.minim.serialiser.serialise(parseResult), null, 2);
+      const result = JSON.stringify(fury.minim.serialiser.serialise(parseResult), null, 2);
 
-    if (process.env.GENERATE) {
-      fs.writeFileSync(`${file}.json`, result);
-    }
+      if (process.env.GENERATE) {
+        fs.writeFileSync(`${file}.json`, result);
+      }
 
-    const expected = fs.readFileSync(`${file}.json`, 'utf-8');
-    expect(result).to.equal(expected);
+      const expected = fs.readFileSync(`${file}.json`, 'utf-8');
+      expect(result).to.equal(expected);
+    });
   });
 });
