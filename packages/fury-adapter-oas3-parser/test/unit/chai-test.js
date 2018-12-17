966dde42839190ee7fbd328997c21fd46ab823c1
Jiří Kratochvíl <kratochvil@klok.cz>
2018-12-17 12:53:36 +0100

    chore: adress review

--- packages/fury-adapter-oas3-parser/test/unit/chai-test.js
+++ packages/fury-adapter-oas3-parser/test/unit/chai-test.js
@@ -1,4 +1,3 @@
-
 const { expect } = require('chai');
 const { Fury } = require('fury');
 
--- packages/fury-adapter-oas3-parser/test/unit/parser/oas/parseOpenAPIObject-test.js
+++ packages/fury-adapter-oas3-parser/test/unit/parser/oas/parseOpenAPIObject-test.js
@@ -214,7 +214,5 @@ describe('#parseOpenAPIObject', () => {
     const result = parse(minim, object);
 
     expect(result.warnings.isEmpty).to.be.true;
-    // FIXME: there are errors - is it correct?
-    // expect(result).to.not.contain.annnotations;
   });
 });
