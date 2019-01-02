e8692a5d7107f6f53bec38c41a5e21e95f35f745
Thomas Jandecka <thomas.jandecka@oracle.com>
2019-01-02 14:38:59 +0100

    feat: sane defaults for unsupported YAML types

--- packages/fury-adapter-oas3-parser/lib/parser/parseYAML.js
+++ packages/fury-adapter-oas3-parser/lib/parser/parseYAML.js
@@ -1,44 +1,82 @@
+/* eslint-disable no-use-before-define */
+
 const yaml = require('yaml-js');
+const { createWarning } = require('./annotations');
 
-function parse(source, namespace) {
-  function convert(node) {
-    let element;
+function extractSourceMap(node, namespace) {
+  return new namespace.elements.SourceMap([
+    [
+      node.start_mark.pointer,
+      node.end_mark.pointer - node.start_mark.pointer,
+    ],
+  ]);
+}
 
-    if (node.tag === 'tag:yaml.org,2002:map') {
-      element = new namespace.elements.Object(node.value.map((nodes) => {
-        const key = convert(nodes[0]);
-        const value = convert(nodes[1]);
-        return new namespace.elements.Member(key, value);
-      }));
-    } else if (node.tag === 'tag:yaml.org,2002:seq') {
-      element = new namespace.elements.Array(node.value.map(convert));
-    } else if (node.tag === 'tag:yaml.org,2002:str') {
-      element = new namespace.elements.String(node.value);
-    } else if (node.tag === 'tag:yaml.org,2002:int' || node.tag === 'tag:yaml.org,2002:float') {
-      element = new namespace.elements.Number(Number(node.value));
-    } else if (node.tag === 'tag:yaml.org,2002:bool') {
-      element = new namespace.elements.Boolean(Boolean(node.value));
-    } else if (node.tag === 'tag:yaml.org,2002:null') {
-      element = new namespace.elements.Null();
-    } else {
-      // FIXME: Unsupported: binary, timestamp, omap, pairs, set
-      throw new Error('Unsupported YAML node');
-    }
+function copySourceMap(node, element, namespace) {
+  element.attributes.set('sourceMap', new namespace.elements.Array([extractSourceMap(node, namespace)]));
+}
 
-    if (element) {
-      const sourceMap = new namespace.elements.SourceMap([
-        [
-          node.start_mark.pointer,
-          node.end_mark.pointer - node.start_mark.pointer,
-        ],
-      ]);
+function yamlToObject(node, annotations, namespace) {
+  return new namespace.elements.Object(node.value.map((nodes) => {
+    const key = convert(nodes[0], annotations, namespace);
+    const value = convert(nodes[1], annotations, namespace);
+    return new namespace.elements.Member(key, value);
+  }));
+}
 
-      element.attributes.set('sourceMap', new namespace.elements.Array([sourceMap]));
-    }
+function yamlToArray(node, annotations, namespace) {
+  return new namespace.elements.Array(node.value.map(nodes => convert(nodes, annotations, namespace)));
+}
 
-    return element;
+function convert(node, annotations, namespace) {
+  let element;
+
+  if (node.tag === 'tag:yaml.org,2002:map') {
+    element = yamlToObject(node, annotations, namespace);
+    copySourceMap(node, element, namespace);
+  } else if (node.tag === 'tag:yaml.org,2002:seq') {
+    element = yamlToArray(node, annotations, namespace);
+    copySourceMap(node, element, namespace);
+  } else if (node.tag === 'tag:yaml.org,2002:str') {
+    element = new namespace.elements.String(node.value);
+    copySourceMap(node, element, namespace);
+  } else if (node.tag === 'tag:yaml.org,2002:int' || node.tag === 'tag:yaml.org,2002:float') {
+    element = new namespace.elements.Number(Number(node.value));
+    copySourceMap(node, element, namespace);
+  } else if (node.tag === 'tag:yaml.org,2002:bool') {
+    element = new namespace.elements.Boolean(Boolean(node.value));
+    copySourceMap(node, element, namespace);
+  } else if (node.tag === 'tag:yaml.org,2002:null') {
+    element = new namespace.elements.Null();
+    copySourceMap(node, element, namespace);
+  } else if (node.tag === 'tag:yaml.org,2002:binary') {
+    element = new namespace.elements.String(node.value);
+    copySourceMap(node, element, namespace);
+    annotations.push(createWarning(namespace, 'Interpreting YAML !!binary as string', element));
+  } else if (node.tag === 'tag:yaml.org,2002:timestamp') {
+    element = new namespace.elements.String(node.value);
+    copySourceMap(node, element, namespace);
+    annotations.push(createWarning(namespace, 'Interpreting YAML !!timestamp as string', element));
+  } else if (node.tag === 'tag:yaml.org,2002:omap') {
+    element = yamlToObject(node, annotations, namespace);
+    copySourceMap(node, element, namespace);
+    annotations.push(createWarning(namespace, 'Interpreting YAML !!omap as object', element));
+  } else if (node.tag === 'tag:yaml.org,2002:pairs') {
+    element = yamlToObject(node, annotations, namespace);
+    copySourceMap(node, element, namespace);
+    annotations.push(createWarning(namespace, 'Interpreting YAML !!pairs as object', element));
+  } else if (node.tag === 'tag:yaml.org,2002:set') {
+    element = yamlToArray(node, annotations, namespace);
+    copySourceMap(node, element, namespace);
+    annotations.push(createWarning(namespace, 'Interpreting YAML !!set as array', element));
+  } else {
+    throw new Error('Unsupported YAML node');
   }
 
+  return element;
+}
+
+function parse(source, namespace) {
   const parseResult = new namespace.elements.ParseResult();
   let ast;
 
@@ -59,11 +97,15 @@ function parse(source, namespace) {
     return parseResult;
   }
 
-  const result = convert(ast);
+  const annotations = [];
+
+  const result = convert(ast, annotations, namespace);
   result.freeze();
+
   parseResult.push(result);
+  parseResult.content = parseResult.content.concat(annotations);
+
   return parseResult;
 }
-
 
 module.exports = parse;
--- packages/fury-adapter-oas3-parser/test/unit/parser/parseYAML-test.js
+++ packages/fury-adapter-oas3-parser/test/unit/parser/parseYAML-test.js
@@ -109,4 +109,157 @@ describe('#parseYAML', () => {
     expect(member.value.toValue()).to.equal('value');
     expect(member.value).to.have.sourceMap([[5, 5]]);
   });
+
+  it('can parse a binary into a string element', () => {
+    const element = parseYAML('!!binary "NDIK"', namespace);
+
+    expect(element).to.be.instanceof(namespace.elements.ParseResult);
+    expect(element.length).to.equal(2);
+
+    expect(element.first).to.be.instanceof(namespace.elements.String);
+    expect(element.first.toValue()).to.equal('NDIK');
+    expect(element.first).to.have.sourceMap([[0, 15]]);
+
+    expect(element.second).to.be.instanceof(namespace.elements.Annotation);
+    expect(element.second.toValue()).to.equal('Interpreting YAML !!binary as string');
+    expect(element.second).to.have.sourceMap([[0, 15]]);
+  });
+
+  it('can parse a timestamp into a string element', () => {
+    const element = parseYAML('1991-09-14 3:00:00.00 +2', namespace);
+
+    expect(element).to.be.instanceof(namespace.elements.ParseResult);
+    expect(element.length).to.equal(2);
+
+    expect(element.first).to.be.instanceof(namespace.elements.String);
+    expect(element.first.toValue()).to.equal('1991-09-14 3:00:00.00 +2');
+    expect(element.first).to.have.sourceMap([[0, 24]]);
+
+    expect(element.second).to.be.instanceof(namespace.elements.Annotation);
+    expect(element.second.toValue()).to.equal('Interpreting YAML !!timestamp as string');
+    expect(element.second).to.have.sourceMap([[0, 24]]);
+  });
+
+  it('can parse an omap into an object element', () => {
+    const element = parseYAML('!!omap\n  key: value', namespace);
+
+    expect(element).to.be.instanceof(namespace.elements.ParseResult);
+    expect(element.length).to.equal(2);
+
+    const object = element.first;
+
+    expect(object).to.be.instanceof(namespace.elements.Object);
+    expect(object.toValue()).to.deep.equal({ key: 'value' });
+    expect(object).to.have.sourceMap([[0, 19]]);
+
+    const member = object.first;
+
+    expect(member).to.be.instanceof(namespace.elements.Member);
+
+    expect(member.key).to.be.instanceof(namespace.elements.String);
+    expect(member.key.toValue()).to.equal('key');
+    expect(member.key).to.have.sourceMap([[9, 3]]);
+
+    expect(member.value).to.be.instanceof(namespace.elements.String);
+    expect(member.value.toValue()).to.equal('value');
+    expect(member.value).to.have.sourceMap([[14, 5]]);
+
+    const warning = element.second;
+    expect(warning).to.be.instanceof(namespace.elements.Annotation);
+    expect(warning.toValue()).to.equal('Interpreting YAML !!omap as object');
+    expect(warning).to.have.sourceMap([[0, 19]]);
+  });
+
+  it('can parse a pairs into an object element', () => {
+    const element = parseYAML('!!pairs\n  key: value', namespace);
+
+    expect(element).to.be.instanceof(namespace.elements.ParseResult);
+    expect(element.length).to.equal(2);
+
+    const object = element.first;
+
+    expect(object).to.be.instanceof(namespace.elements.Object);
+    expect(object.toValue()).to.deep.equal({ key: 'value' });
+    expect(object).to.have.sourceMap([[0, 20]]);
+
+    const member = object.first;
+
+    expect(member).to.be.instanceof(namespace.elements.Member);
+
+    expect(member.key).to.be.instanceof(namespace.elements.String);
+    expect(member.key.toValue()).to.equal('key');
+    expect(member.key).to.have.sourceMap([[10, 3]]);
+
+    expect(member.value).to.be.instanceof(namespace.elements.String);
+    expect(member.value.toValue()).to.equal('value');
+    expect(member.value).to.have.sourceMap([[15, 5]]);
+
+    const warning = element.second;
+    expect(warning).to.be.instanceof(namespace.elements.Annotation);
+    expect(warning.toValue()).to.equal('Interpreting YAML !!pairs as object');
+    expect(warning).to.have.sourceMap([[0, 20]]);
+  });
+
+  it('can parse a set into an array element', () => {
+    const element = parseYAML('!!set\n  - one\n  - two', namespace);
+
+    expect(element).to.be.instanceof(namespace.elements.ParseResult);
+    expect(element.length).to.equal(2);
+
+    const arry = element.first;
+
+    expect(arry).to.be.instanceof(namespace.elements.Array);
+    expect(arry.toValue()).to.deep.equal(['one', 'two']);
+    expect(arry).to.have.sourceMap([[0, 21]]);
+
+    const entryOne = arry.get(0);
+
+    expect(entryOne).to.be.instanceof(namespace.elements.String);
+    expect(entryOne.toValue()).to.equal('one');
+    expect(entryOne).to.have.sourceMap([[10, 3]]);
+
+    const entryTwo = arry.get(1);
+
+    expect(entryTwo).to.be.instanceof(namespace.elements.String);
+    expect(entryTwo.toValue()).to.equal('two');
+    expect(entryTwo).to.have.sourceMap([[18, 3]]);
+
+    const warning = element.second;
+    expect(warning).to.be.instanceof(namespace.elements.Annotation);
+    expect(warning.toValue()).to.equal('Interpreting YAML !!set as array');
+    expect(warning).to.have.sourceMap([[0, 21]]);
+  });
+
+  it('can accumulate annotations during YAML translation', () => {
+    const element = parseYAML('!!omap\n  key: !!binary "NDIK"', namespace);
+
+    expect(element).to.be.instanceof(namespace.elements.ParseResult);
+    expect(element.length).to.equal(3);
+
+    const object = element.first;
+
+    expect(object).to.be.instanceof(namespace.elements.Object);
+    expect(object.toValue()).to.deep.equal({ key: 'NDIK' });
+    expect(object).to.have.sourceMap([[0, 29]]);
+
+    const member = object.first;
+
+    expect(member.key).to.be.instanceof(namespace.elements.String);
+    expect(member.key.toValue()).to.equal('key');
+    expect(member.key).to.have.sourceMap([[9, 3]]);
+
+    expect(member.value).to.be.instanceof(namespace.elements.String);
+    expect(member.value.toValue()).to.equal('NDIK');
+    expect(member.value).to.have.sourceMap([[14, 15]]);
+
+    const innerWarning = element.get(1);
+    expect(innerWarning).to.be.instanceof(namespace.elements.Annotation);
+    expect(innerWarning.toValue()).to.equal('Interpreting YAML !!binary as string');
+    expect(innerWarning).to.have.sourceMap([[14, 15]]);
+
+    const outerWarning = element.get(2);
+    expect(outerWarning).to.be.instanceof(namespace.elements.Annotation);
+    expect(outerWarning.toValue()).to.equal('Interpreting YAML !!omap as object');
+    expect(outerWarning).to.have.sourceMap([[0, 29]]);
+  });
 });
