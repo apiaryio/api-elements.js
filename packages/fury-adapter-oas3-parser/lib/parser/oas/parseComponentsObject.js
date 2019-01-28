096adc303c09304e09670d46d364bfc39b1b8918
Kyle Fuller <kyle@fuller.li>
2019-01-28 20:13:10 +0000

    refactor(oas3): make component parsing generic
    We no longer have to write specific parsers for each component type,
    instead we can now create a components parser from other generic parser
    types (schema parser, parameter parser etc).

--- packages/fury-adapter-oas3-parser/lib/parser/oas/parseComponentsObject.js
+++ packages/fury-adapter-oas3-parser/lib/parser/oas/parseComponentsObject.js
@@ -16,6 +16,53 @@ const name = 'Components Object';
 const unsupportedKeys = ['responses', 'examples', 'requestBodies', 'headers', 'securitySchemes', 'links', 'callbacks'];
 const isUnsupportedKey = R.anyPass(R.map(hasKey, unsupportedKeys));
 
+const valueIsObject = R.compose(isObject, getValue);
+
+/**
+ * Is the given parse result empty (i.e, contains no elements other than annotations)
+ *
+ * @param parseResult {ParseResult}
+ * @returns boolean
+ */
+const isParseResultEmpty = parseResult => R.reject(isAnnotation, parseResult).isEmpty;
+
+/**
+ * Parse a component member
+ *
+ * This function takes another parser that is capable of parsing the specific
+ * component type (schema parser, parameter parser etc) and parses a
+ * component using the given parser. It will return a parse result of member.
+ *
+ * In the cases that the given member cannot be parsed, it will result in
+ * returning a member element to represent the key without a value.
+ *
+ * @param context
+ * @param parser {function}
+ * @param member {Member} - Member element to represent the component,
+ *   member key contains the reusable component name, value represents
+ *   the reusable component
+ *
+ * @returns ParseResult
+ */
+const parseComponentMember = R.curry((context, parser, member) => {
+  // Create a Member Element with `member.key` as the key
+  const Member = R.constructN(2, context.namespace.elements.Member)(member.key);
+
+  const parseResult = pipeParseResult(context.namespace,
+    parser(context),
+    Member)(member.value);
+
+  if (isParseResultEmpty(parseResult)) {
+    // parse result does not contain a member, that's because parsing a
+    // component has failed. We want to store the member without value in
+    // this case so that we can correctly know if a component with the name
+    // existed during dereferencing.
+    parseResult.unshift(Member(undefined));
+  }
+
+  return parseResult;
+});
+
 /**
  * Parse Components Object
  *
@@ -28,45 +75,39 @@ const isUnsupportedKey = R.anyPass(R.map(hasKey, unsupportedKeys));
 function parseComponentsObject(context, element) {
   const { namespace } = context;
 
-  const validateIsObject = key => R.unless(isObject,
-    createWarning(namespace, `'${name}' '${key}' is not an object`));
+  const createMemberValueNotObjectWarning = member => createWarning(namespace,
+    `'${name}' '${member.key.toValue()}' is not an object`, member.value);
+  const validateIsObject = R.unless(valueIsObject, createMemberValueNotObjectWarning);
 
-  const parseSchemaMember = member => pipeParseResult(namespace,
-    R.compose(parseSchemaObject(context), getValue),
-    (dataStructure) => {
-      // eslint-disable-next-line no-param-reassign
-      dataStructure.content.id = member.key.clone();
-      return dataStructure;
-    })(member);
+  /**
+   * Parses a member representing a component object (such as an object
+   * representing the parameter components)
+   *
+   * @param parser {function}
+   * @param member {Member}
+   *
+   * @returns ParseResult
+   */
+  const parseComponentObjectMember = (parser) => {
+    const parseMember = parseComponentMember(context, parser);
 
-  const parseSchemasObject = pipeParseResult(namespace,
-    validateIsObject('schemas'),
-    parseObject(context, name, parseSchemaMember));
-
-  const parseParametersObjectMember = (member) => {
-    // Create a Member Element with `member.key` as the key
-    const Member = R.constructN(2, namespace.elements.Member)(member.key);
-    const parseResult = parseParameterObject(context, member.value);
-    // Wrap non-annotation elements in member element
-    const result = R.map(R.unless(isAnnotation, Member), parseResult);
-
-    if (result.annotations.length === result.length) {
-      // failed to parse parameter, let's store a member without a value
-      // in parameters components section so dereferencing can know if the
-      // parameter existing under the key name
-      result.unshift(new namespace.elements.Member(member.key));
-    }
-
-    return result;
+    return pipeParseResult(context.namespace,
+      validateIsObject,
+      R.compose(parseObject(context, name, parseMember), getValue));
   };
 
-  const parseParametersObject = pipeParseResult(namespace,
-    validateIsObject('parameters'),
-    parseObject(context, name, parseParametersObjectMember));
+  // eslint-disable-next-line no-param-reassign
+  const setDataStructureId = (dataStructure, key) => { dataStructure.content.id = key.clone(); };
+  const parseSchemas = pipeParseResult(namespace,
+    parseComponentObjectMember(parseSchemaObject),
+    (object) => {
+      object.forEach(setDataStructureId);
+      return object;
+    });
 
   const parseMember = R.cond([
-    [hasKey('schemas'), R.compose(parseSchemasObject, getValue)],
-    [hasKey('parameters'), R.compose(parseParametersObject, getValue)],
+    [hasKey('schemas'), parseSchemas],
+    [hasKey('parameters'), parseComponentObjectMember(parseParameterObject)],
     [isUnsupportedKey, createUnsupportedMemberWarning(namespace, name)],
 
     // FIXME Support exposing extensions into parse result
