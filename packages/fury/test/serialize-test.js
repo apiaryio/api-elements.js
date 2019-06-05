dad72c74b2e86150cc7c4713c4c2a2538f86074a
Kyle Fuller <kyle@fuller.li>
2019-06-05 11:12:52 +0100

    test(core): add coverage for serialize

---
+++ packages/fury/test/serialize-test.js
@@ -0,0 +1,49 @@
+const { expect } = require('chai');
+const { Fury } = require('../lib/fury');
+
+describe('Serialize', () => {
+  it('errors with unknown mediaType', (done) => {
+    const fury = new Fury();
+    const api = new fury.minim.elements.Category();
+
+    fury.serialize({ api, mediaType: 'application/unregistered' }, (error, result) => {
+      expect(error.message).to.equal('Media type did not match any registered serializer!');
+      expect(result).to.be.undefined;
+      done();
+    });
+  });
+
+  it('errors with matching erroring adapter', (done) => {
+    const fury = new Fury();
+    fury.use({
+      name: 'json',
+      mediaTypes: ['application/json'],
+      serialize: () => Promise.reject(new Error('failed to serialize')),
+    });
+
+    const api = new fury.minim.elements.Category();
+
+    fury.serialize({ api, mediaType: 'application/json' }, (error, result) => {
+      expect(error.message).to.equal('failed to serialize');
+      expect(result).to.be.undefined;
+      done();
+    });
+  });
+
+  it('can serialize with matching adapter', (done) => {
+    const fury = new Fury();
+    fury.use({
+      name: 'json',
+      mediaTypes: ['application/json'],
+      serialize: ({ api, minim }) => Promise.resolve(JSON.stringify(minim.serialiser.serialise(api))),
+    });
+
+    const api = new fury.minim.elements.Category();
+
+    fury.serialize({ api, mediaType: 'application/json' }, (error, result) => {
+      expect(error).to.be.null;
+      expect(result).to.equal('{"element":"category"}');
+      done();
+    });
+  });
+});
