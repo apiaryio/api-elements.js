efebc6bbad97eae1916652707db462983052585d
Kyle Fuller <kyle@fuller.li>
2019-07-02 17:56:13 +0100

    ci(project): setup a smoke test

---
+++ .circleci/smoke.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+set -e
+
+TMPDIR="$(mktemp -d 2>/dev/null || mktemp -d -t 'apiejs-smoketest')"
+API_ELEMENTS="$(pwd)"
+
+create_package() {
+  cd "$TMPDIR" && npm init --yes && cd "$API_ELEMENTS"
+}
+
+install() {
+  PACKAGE="$1"
+
+  TARBALL="$(cd packages/$PACKAGE && npm pack)"
+  mv "packages/$PACKAGE/$TARBALL" "$TMPDIR"
+
+  cd "$TMPDIR" && npm install "$TARBALL" && cd "$API_ELEMENTS"
+}
+
+echo "Setting up Fury in $TMPDIR"
+
+create_package
+install fury
+install fury-adapter-apib-parser
+install fury-adapter-oas3-parser
+install fury-adapter-apiary-blueprint-parser
+install fury-adapter-apib-serializer
+install fury-cli
+
+cd "$TMPDIR"
+npx fury --help
+npx fury --version
+
+# Parse Apiary Blueprint
+cat << EOF | npx fury -
+HOST: http://www.google.com/
+
+--- Sample API v2 ---
+---
+Welcome to the our sample API documentation. All comments can be written in (support [Markdown](http://daringfireball.net/projects/markdown/syntax) syntax)
+---
+EOF
+
+# Parse API Blueprint
+cat << EOF | npx fury -
+FORMAT: 1A
+
+# GET /
+
++ Response 204
+EOF
+
+# Parse OpenAPI 2.0
+cat << EOF | npx fury -
+swagger: '2.0'
+info:
+  title: My API
+  version: 1.0.0
+paths: {}
+EOF
+
+# Parse OpenAPI 3.0
+cat << EOF | npx fury -
+openapi: 3.0.2
+info:
+  title: My API
+  version: 1.0.0
+paths: {}
+EOF
+
+rm -fr "$TMPDIR"
