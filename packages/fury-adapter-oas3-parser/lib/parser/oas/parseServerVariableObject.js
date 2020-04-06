d8876bdb8d61bc531bda76deeddb8096a379ff02
Marco Friso <marco.friso@oracle.com>
2020-04-06 15:59:38 +0200

    feat(oas3): implement Server Variable object

---
+++ packages/fury-adapter-oas3-parser/lib/parser/oas/parseServerVariableObject.js
@@ -0,0 +1,50 @@
+const R = require('ramda');
+const { createWarning } = require('../../elements');
+const {
+  createInvalidMemberWarning,
+} = require('../annotations');
+const {
+  isObject, hasKey, isExtension, getValue,
+} = require('../../predicates');
+const parseObject = require('../parseObject');
+const parseString = require('../parseString');
+const parseEnum = require('../parseEnum');
+const pipeParseResult = require('../../pipeParseResult');
+
+const name = 'Server Variable Object';
+const requiredKeys = ['default'];
+
+const parseMember = context => R.cond([
+  [hasKey('enum'), R.compose(parseEnum(context, name), getValue)],
+  [hasKey('default'), parseString(context, name, true)],
+  [hasKey('description'), parseString(context, name, false)],
+  [isExtension, () => new context.namespace.elements.ParseResult()],
+  [R.T, createInvalidMemberWarning(context.namespace, name)],
+]);
+
+/**
+ * Parse the OpenAPI 'Server Variable Object' (`#/server/server-variable`)
+ * @see http://spec.openapis.org/oas/v3.0.3#server-variable-object
+ * @returns ParseResult<Member>
+ * @private
+ */
+const parseServerVariableObject = context => pipeParseResult(context.namespace,
+  R.unless(isObject, createWarning(context.namespace, `'${name}' is not an object`)),
+  parseObject(context, name, parseMember(context), requiredKeys, [], true),
+  (object) => {
+    const variable = new context.namespace.elements.Member();
+
+    variable.default = object.getValue('default');
+
+    if (object.hasKey('description')) {
+      variable.description = object.getValue('description');
+    }
+
+    if (object.hasKey('enum')) {
+      variable.enum = object.get('enum');
+    }
+
+    return variable;
+  });
+
+module.exports = parseServerVariableObject;
---
+++ packages/fury-adapter-oas3-parser/test/unit/parser/oas/parseServerVariableObject-test.js
@@ -0,0 +1,122 @@
+const { Fury } = require('fury');
+const { expect } = require('../../chai');
+
+const parse = require('../../../../lib/parser/oas/parseServerVariableObject');
+const Context = require('../../../../lib/context');
+
+const { minim: namespace } = new Fury();
+
+describe('#parseServerVariableObject', () => {
+  let context;
+  beforeEach(() => {
+    context = new Context(namespace);
+  });
+
+  it('provides warning when server variable is non-object', () => {
+    const serverVariable = new namespace.elements.String();
+
+    const parseResult = parse(context)(serverVariable);
+
+    expect(parseResult.length).to.equal(1);
+    expect(parseResult).to.contain.warning("'Server Variable Object' is not an object");
+  });
+
+  describe('#default', () => {
+    it('warns when server variable object does not contain the default value', () => {
+      const serverVariable = new namespace.elements.Object({
+      });
+
+      const parseResult = parse(context)(serverVariable);
+      expect(parseResult.length).to.equal(1);
+      expect(parseResult).to.contain.warning("'Server Variable Object' is missing required property 'default'");
+    });
+
+    it('warns when default is not a string', () => {
+      const serverVariable = new namespace.elements.Object({
+        default: 1234,
+        description: 'User name',
+      });
+
+      const parseResult = parse(context)(serverVariable);
+      expect(parseResult).to.contain.error("'Server Variable Object' 'default' is not a string");
+    });
+
+    it('parse server variable object with default value', () => {
+      const serverVariable = new namespace.elements.Object({
+        default: 'Mario',
+      });
+
+      const parseResult = parse(context)(serverVariable);
+      expect(parseResult).to.not.contain.annotations;
+
+      const member = parseResult.get(0);
+      expect(member).to.be.instanceof(namespace.elements.Member);
+      expect(member.default).to.be.equal('Mario');
+    });
+  });
+
+  describe('#description', () => {
+    it('warns when description is not a string', () => {
+      const serverVariable = new namespace.elements.Object({
+        default: 'Mario',
+        description: 1234,
+      });
+
+      const parseResult = parse(context)(serverVariable);
+      expect(parseResult.get(0)).to.be.instanceof(namespace.elements.Member);
+      expect(parseResult).to.contain.warning("'Server Variable Object' 'description' is not a string");
+    });
+
+    it('parse server variable object with description', () => {
+      const serverVariable = new namespace.elements.Object({
+        default: 'Mario',
+        description: 'User name',
+      });
+
+      const parseResult = parse(context)(serverVariable);
+      expect(parseResult).to.not.contain.annotations;
+
+      const member = parseResult.get(0);
+      expect(member).to.be.instanceof(namespace.elements.Member);
+
+      const description = member.description.toValue();
+      expect(description).to.be.equal('User name');
+    });
+  });
+
+  describe('#enum', () => {
+    it('warns when enum is not an array', () => {
+      const serverVariable = new namespace.elements.Object({
+        default: 'Mario',
+        enum: 1,
+      });
+      const parseResult = parse(context)(serverVariable);
+
+      expect(parseResult).to.contain.warning(
+        "'Server Variable Object' 'enum' is not an array"
+      );
+    });
+
+    it('parse server variable object with enum', () => {
+      const serverVariable = new namespace.elements.Object({
+        default: 'Mario',
+        enum: [['Tony', 'Nina']],
+      });
+
+      const parseResult = parse(context)(serverVariable);
+      expect(parseResult).to.not.contain.annotations;
+
+      const member = parseResult.get(0);
+      expect(member).to.be.instanceof(namespace.elements.Member);
+
+      const enumElement = member.enum;
+      expect(enumElement).to.be.instanceof(namespace.elements.Enum);
+      expect(enumElement.enumerations.length).to.equal(1);
+
+      const enumeration = enumElement.enumerations.get(0);
+      expect(enumeration).to.be.instanceof(namespace.elements.Array);
+      expect(enumeration.toValue()).to.deep.equal(['Tony', 'Nina']);
+      expect(enumeration.attributes.getValue('typeAttributes')).to.deep.equal(['fixed']);
+    });
+  });
+});
