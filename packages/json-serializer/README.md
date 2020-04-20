aed02c9322221b4fcf6adf025f6b96bfab51e3c0
Kyle Fuller <kyle@fuller.li>
2020-04-20 18:59:19 +0100

    feat(json): add JSON serializer

---
+++ packages/json-serializer/README.md
@@ -0,0 +1,24 @@
+# API Elements: JSON Serializer
+
+## Usage
+
+Takes an API Element data structure, and returns JSON serialized data
+structures, for example:
+
+```javascript
+const { Fury } = require('@apielements/core');
+const jsonSerializer = require('@apielements/json-serializer');
+
+const fury = new Fury();
+fury.use(jsonSerializer);
+
+const name = new fury.minim.elements.String();
+name.attributes.set('default', 'Doe');
+
+const api = new fury.minim.elements.Object({ name });
+const mediaType = 'application/json';
+fury.serialize({ api, mediaType }, (error, body) => {
+  console.log(body);
+  // { "name": "Doe" }
+});
+```
---
+++ packages/json-serializer/test/serializeJSON-test.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const { Fury } = require('@apielements/core');
+const serializeJSON = require('../lib/serializeJSON');
+
+const { minim: namespace } = new Fury();
+
+describe('#serializeJSON', () => {
+  it('can serialize a primitive element with value', () => {
+    const element = new namespace.elements.String('hello world');
+
+    expect(serializeJSON(element)).to.equal('"hello world"');
+  });
+
+  it('can serialize a primitive element with default value', () => {
+    const element = new namespace.elements.String();
+    element.attributes.set('default', 'hello world');
+
+    expect(serializeJSON(element)).to.equal('"hello world"');
+  });
+
+  it('can serialize an element with references via parent tree', () => {
+    const element = new namespace.elements.Element();
+    element.element = 'message';
+
+    new namespace.elements.Category([
+      new namespace.elements.Category([
+        new namespace.elements.String('hello world', { id: 'message' }),
+      ], { classes: ['dataStructures'] }),
+      element,
+    ]).freeze();
+
+    expect(serializeJSON(element)).to.equal('"hello world"');
+  });
+});
