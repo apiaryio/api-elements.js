fcaf2053a521d12f26248688ad405672511fcc02
Marco Friso <marco.friso@oracle.com>
2020-04-22 17:44:00 +0200

    test(oas3): add server variables object validation
    Closes APIARY-6482

--- packages/fury-adapter-oas3-parser/lib/parser/oas/parseServerObject.js
+++ packages/fury-adapter-oas3-parser/lib/parser/oas/parseServerObject.js
@@ -15,6 +15,38 @@ const parseServerVariableObject = require('./parseServerVariableObject');
 const name = 'Server Object';
 const requiredKeys = ['url'];
 
+const validateVariableInURL = (context, object) => {
+  const url = object.getValue('url');
+  const variables = object.get('variables');
+  const parseResult = new context.namespace.elements.ParseResult();
+
+  const variablesKeys = variables.content.map(variable => variable.key.toValue());
+  const urlVariables = url
+    .match(/{(.*?)}/g)
+    .map(x => x.replace(/[{}]/g, ''));
+
+  // if you define a variable that is not in URL it warns (and the variable is discarded).
+  variablesKeys.forEach((key) => {
+    if (urlVariables.indexOf(key) === -1) {
+      parseResult.push(createWarning(context.namespace,
+        `Server variable '${key}' is not present in the URL and will be discarted`, variables));
+
+      variables.remove(key);
+    }
+  });
+
+  // if you place a variable in the URL and its not in variables you get a warning that the variable is missing.
+  urlVariables.forEach((key) => {
+    if (!variables.hasKey(key)) {
+      parseResult.push(createWarning(context.namespace,
+        `URL variable '${key}' is missing within the server variables`, variables));
+    }
+  });
+
+  parseResult.push(object);
+  return parseResult;
+};
+
 const parseMember = context => R.cond([
   [hasKey('description'), parseString(context, name, false)],
   [hasKey('url'), parseString(context, name, true)],
@@ -22,6 +54,8 @@ const parseMember = context => R.cond([
   [isExtension, () => new context.namespace.elements.ParseResult()],
   [R.T, createInvalidMemberWarning(context.namespace, name)],
 ]);
+
+const hasVariables = object => object.hasKey('variables');
 
 /**
  * Parse the OpenAPI 'Server Object' (`#/server`)
@@ -32,6 +66,7 @@ const parseMember = context => R.cond([
 const parseServerObject = context => pipeParseResult(context.namespace,
   R.unless(isObject, createWarning(context.namespace, `'${name}' is not an object`)),
   parseObject(context, name, parseMember(context), requiredKeys, [], true),
+  R.when(hasVariables, R.curry(validateVariableInURL)(context)),
   (object) => {
     const resource = new context.namespace.elements.Resource();
 
