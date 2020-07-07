4a3e5062ae50aca7d513b4f9800a53fc735888fb
Natalie <natalie.mikesova@oracle.com>
2020-07-07 10:17:03 +0200

    test(form): add data structures tests

--- packages/form-serializer/test/serializeForm-test.js
+++ packages/form-serializer/test/serializeForm-test.js
@@ -10,4 +10,25 @@ describe('#serializeForm', () => {
 
     expect(serializeForm({ api: element })).to.equal('--BOUNDARY\r\nContent-Disposition: form-data; name="undefined"\r\n\r\nHello world\r\n--BOUNDARY--\r\n');
   });
+
+  it('can serialize a primitive element with a default value', () => {
+    const element = new namespace.elements.String();
+    element.attributes.set('default', 'Hello world');
+
+    expect(serializeForm({ api: element })).to.equal('--BOUNDARY\r\nContent-Disposition: form-data; name="undefined"\r\n\r\nHello world\r\n--BOUNDARY--\r\n');
+  });
+
+  it('can serialize an element with references via parent tree', () => {
+    const element = new namespace.elements.Element();
+    element.element = 'message';
+
+    new namespace.elements.Category([
+      new namespace.elements.Category([
+        new namespace.elements.String('Hello world', { id: 'message' }),
+      ], { classes: ['dataStructures'] }),
+      element,
+    ]).freeze();
+
+    expect(serializeForm({ api: element })).to.equal('--BOUNDARY\r\nContent-Disposition: form-data; name="undefined"\r\n\r\nHello world\r\n--BOUNDARY--\r\n');
+  });
 });
