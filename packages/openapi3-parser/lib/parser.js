3c0427b611ba30f516035dbfa7f1b3c4ae3971c3
Kyle Fuller <kyle@fuller.li>
2020-07-30 14:13:03 +0100

    refactor(oas3): move source map filtering into main parser
    This logic does not belong in parseOpenAPIObject as it isn't directly
    related to parsing the OpenAPI Object and instead a general parsing
    behaviour added after parsing components.

--- packages/openapi3-parser/lib/parser.js
+++ packages/openapi3-parser/lib/parser.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+
 const R = require('ramda');
 const parseYAML = require('./parser/parseYAML');
 
@@ -9,12 +11,81 @@ const parseOpenAPIObject = require('./parser/oas/parseOpenAPIObject');
 
 const isObjectOrAnnotation = R.either(isObject, isAnnotation);
 
+const recurseSkippingAnnotations = R.curry((visitor, e) => {
+  if (isAnnotation(e)) {
+    return e;
+  }
+  if (e) {
+    if (!e.element) {
+      return e;
+    }
+
+    visitor(e);
+
+    if (e._attributes) {
+      e.attributes.forEach((value, key, member) => {
+        recurseSkippingAnnotations(visitor, member);
+      });
+    }
+    if (e._meta) {
+      e.meta.forEach((value, key, member) => {
+        recurseSkippingAnnotations(visitor, member);
+      });
+    }
+    if (e.content) {
+      if (Array.isArray(e.content)) {
+        e.content.forEach((value) => {
+          recurseSkippingAnnotations(visitor, value);
+        });
+      }
+      if (e.content.key) {
+        recurseSkippingAnnotations(visitor, e.content.key);
+        if (e.content.value) {
+          recurseSkippingAnnotations(visitor, e.content.value);
+        }
+      }
+      if (e.content.element) {
+        recurseSkippingAnnotations(visitor, e.content);
+      }
+    }
+  }
+  return e;
+});
+
+function removeSourceMap(e) {
+  if (e._attributes) {
+    e.attributes.remove('sourceMap');
+  }
+}
+
+function removeColumnLine(result) {
+  if (result._attributes) {
+    const sourceMaps = result.attributes.get('sourceMap');
+    if (sourceMaps) {
+      sourceMaps.content.forEach((sourceMap) => {
+        sourceMap.content.forEach((sourcePoint) => {
+          sourcePoint.content.forEach((sourceCoordinate) => {
+            if (sourceCoordinate._attributes) {
+              sourceCoordinate.attributes.remove('line');
+              sourceCoordinate.attributes.remove('column');
+            }
+          });
+        });
+      });
+    }
+  }
+}
+
+const filterColumnLine = recurseSkippingAnnotations(removeColumnLine);
+const filterSourceMaps = recurseSkippingAnnotations(removeSourceMap);
+
 function parse(source, context) {
   const document = parseYAML(source, context);
 
   const parseDocument = pipeParseResult(context.namespace,
     R.unless(isObjectOrAnnotation, createError(context.namespace, 'Source document is not an object')),
-    R.unless(isAnnotation, parseOpenAPIObject(context)));
+    R.unless(isAnnotation, parseOpenAPIObject(context)),
+    context.options.generateSourceMap ? filterColumnLine : filterSourceMaps);
 
   return R.chain(parseDocument, document);
 }
